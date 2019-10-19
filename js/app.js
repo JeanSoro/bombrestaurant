8dcf449dafced839ddd5063a81dd76a81a0d9cce
Jean <jean.soro007@gmail.com>
2019-10-19 15:18:27 -0400

    set up image food  gallery pop up function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,16 @@ $(document).ready(function () {
   $(".banner, .info").ripples({
     dropRadius: 25,
     perturbance: 0.6,
+  });
 
+  $('.parent-container').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+
+    gallery: {
+      enabled: true
+    }
+    // other options
   });
 
 
